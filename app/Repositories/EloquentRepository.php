8cafc6fc5160d1993b9347e4733c1e128d52e191
root <root@localhost>
2021-04-14 19:03:44 +0000

    eloquent repository fix

--- app/Repositories/EloquentRepository.php
+++ app/Repositories/EloquentRepository.php
@@ -42,7 +42,7 @@ class EloquentRepository
      */
     public function update($data = []): mixed
     {
-        $baseQuery = static::$model::where('id',intval($data['id']));
+        $baseQuery = static::$model::where('id',intval(($data['id'] ?? 0)));
         $update = $baseQuery->update($data);
 
         if($update=='0'){
