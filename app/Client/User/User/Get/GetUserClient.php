76eae706e4097de906b6d7d8205d47a7b225c291
root <root@localhost>
2021-04-19 08:37:03 +0000

    user client fix

--- app/Client/User/User/Get/GetUserClient.php
+++ app/Client/User/User/Get/GetUserClient.php
@@ -33,6 +33,6 @@ class GetUserClient extends Client
      * @var array
      */
     protected array $rule = [
-        'restaurant_code' => 'required|integer'
+        //'id' => 'required|integer'
     ];
 }
