477a2d67042a61444715be86ce8aeed0f2871f44
root <root@localhost>
2021-04-19 13:41:12 +0000

    command client fix

---
+++ app/Console/Commands/ClientCommand.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Client\Client;
+use App\Client\ClientAutoGeneratorTrait;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Nette\PhpGenerator\Literal;
+use Nette\PhpGenerator\PhpNamespace;
+
+class ClientCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:client {dir} {client} {method}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $clientDir = ucfirst($this->argument('dir'));
+        $clientName = ucfirst($this->argument('client'));
+        $method = ucfirst($this->argument('method'));
+
+        $dirPath = app_path('Client').''.DIRECTORY_SEPARATOR.''.$clientDir;
+        $namePath = app_path('Client').''.DIRECTORY_SEPARATOR.''.$clientDir.''.DIRECTORY_SEPARATOR.''.$clientName;
+        $methodPath = app_path('Client').''.DIRECTORY_SEPARATOR.''.$clientDir.''.DIRECTORY_SEPARATOR.''.$clientName.''.DIRECTORY_SEPARATOR.''.$method;
+        $namespace = 'App\Client\\'.$clientDir.'\\'.$clientName.'\\'.$method;
+        $className =  $method.'Client'.$clientName;
+        $namespaceClass = 'App\Client\\'.$clientDir.'\\'.$clientName.'\\'.$method.'\\'.$className;
+        $fileClass = app_path('Client').''.DIRECTORY_SEPARATOR.''.$clientDir.''.DIRECTORY_SEPARATOR.''.$clientName.''.DIRECTORY_SEPARATOR.''.$method.''.DIRECTORY_SEPARATOR.''.$className;
+
+
+        if(!file_exists($dirPath)){
+            File::makeDirectory($dirPath);
+        }
+
+        if(!file_exists($namePath)){
+            File::makeDirectory($namePath);
+        }
+
+        if(!file_exists($methodPath)){
+            File::makeDirectory($methodPath);
+        }
+
+
+        $generator = new PhpNamespace($namespace);
+        $generator = $generator->addTrait($traitName = 'GeneratorTrait');
+        $generator->addProperty('generators',[])->setType('array')->setProtected()
+            ->addComment('get auto generator for client')
+            ->addComment('')
+            ->addComment('@return array');
+
+        $generator->addProperty('dontOverWriteGenerators',[])->setType('array')->setProtected()
+            ->addComment('get dont overwrite generator for client')
+            ->addComment('')
+            ->addComment('@return array');
+
+        touch($file = $methodPath.''.DIRECTORY_SEPARATOR.''.$traitName.'.php');
+        $content = '<?php '.PHP_EOL.''.PHP_EOL.'namespace '.$namespace.'; '.PHP_EOL.''.PHP_EOL.''.$generator;
+        File::put($file,$content);
+
+        $generatorClass = new PhpNamespace($namespace);
+        $generatorClass->addUse(Client::class);
+        $generatorClass->addUse(ClientAutoGeneratorTrait::class);
+        $classGenerator = $generatorClass->addClass($className)->setExtends(Client::class);
+        $classGenerator->addTrait($namespace.'\\'.$traitName);
+        $classGenerator->addTrait(ClientAutoGeneratorTrait::class);
+
+        $classGenerator->addProperty('capsule',[])->setType('array')->setProtected()
+            ->addComment('get capsule for client')
+            ->addComment('')
+            ->addComment('@var array');
+
+        $classGenerator->addProperty('model',[])->setType('array')->setProtected()
+            ->addComment('get model entity validation')
+            ->addComment('')
+            ->addComment('@var array|string[]');
+
+        $classGenerator->addProperty('rule',[])->setType('array')->setProtected()
+            ->addComment('get rule for client')
+            ->addComment('')
+            ->addComment('@var array');
+
+        touch($file = $fileClass.'.php');
+        $content = '<?php '.PHP_EOL.''.PHP_EOL.''.$generatorClass;
+        File::put($file,$content);
+
+        $this->warn('client has been created');
+        return 1;
+    }
+}
