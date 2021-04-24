4995ce2985ba7a9cd9e624a235b658334a8504e1
aligurbuz <galiant781@gmail.com>
2021-04-24 17:18:18 +0000

    controller fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,23 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
+     * @var array
+     */
+    protected array $middlewares = ['auth:api','apiKey','accessLogger','response'];
+
+    /**
+     * @var array
+     */
+    protected array $exceptMiddlewares = [
+      'App\Http\Controllers\Auth\LoginController' => ['auth:api']
+    ];
+
+    /**
      * Controller constructor.
      */
     public function __construct()
     {
-        $this->middleware('auth:api');
-        $this->middleware('apiKey');
-        $this->middleware('accessLogger');
-        $this->middleware('response');
+        foreach ($this->middlewares as $middleware){
+            $this->exceptMiddlewares($middleware,function() use($middleware){
+                $this->middleware($middleware);
+            });
+        }
+    }
+
+    /**
+     * @param $middleware
+     * @param callable $callback
+     * @return mixed
+     */
+    private function exceptMiddlewares($middleware,callable $callback): mixed
+    {
+        $calledClass = get_called_class();
+
+        if(
+            isset($this->exceptMiddlewares[$calledClass])
+            && in_array($middleware,$this->exceptMiddlewares[$calledClass])
+        ){
+            return false;
+        }
+
+        return call_user_func($callback);
     }
 }
