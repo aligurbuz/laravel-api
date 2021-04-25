ed7958d870b53e35051245e76c208a2f29bc7935
aligurbuz <galiant781@gmail.com>
2021-04-25 10:01:32 +0000

    repository fix

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\User\Contracts\UserRepositoryContract;
+use App\Repositories\User\Contracts\CommentRepositoryContract;
+
+class Repository
+{
+    /**
+     * get user repository instance
+     *
+     * @return UserRepositoryContract
+     */
+    public static function user() : UserRepositoryContract
+    {
+        return app()->get(UserRepositoryContract::class);
+    }
+
+    /**
+     * get user comment repository instance
+     *
+     * @return CommentRepositoryContract
+     */
+    public static function userComment() : CommentRepositoryContract
+    {
+        return app()->get(CommentRepositoryContract::class);
+    }
+}
