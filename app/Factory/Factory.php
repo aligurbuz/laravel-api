a74a671db281e7704eef3cfe61dc6c93cce79a7e
aligurbuz <galiant781@gmail.com>
2021-04-28 20:43:47 +0000

    factory fix

--- app/Factory/Factory.php
+++ app/Factory/Factory.php
@@ -2,7 +2,6 @@
 
 namespace App\Factory;
 
-use Exception;
 use App\Factory\Owner\Interfaces\OwnerInterface;
 
 /**
@@ -10,34 +9,13 @@ use App\Factory\Owner\Interfaces\OwnerInterface;
  * @method static OwnerInterface owner($arguments = null)
  * @package App\Factory
  */
-class Factory
+class Factory extends FactoryManager
 {
     /**
+     * #this is adapter property for factory model
      * get the called class for factory
      *
      * @var array
      */
     protected static array $adapters = [];
-
-    /**
-     * get call static for factory
-     *
-     * @param string $name
-     * @param array $arguments
-     * @return mixed
-     *
-     * @throws Exception
-     */
-    public static function __callStatic(string $name,array $arguments = []): mixed
-    {
-        $name = ucfirst($name);
-        $adapters = (isset(static::$adapters[$name])) ? static::$adapters[$name] : $name;
-        $factory = 'App\Factory\\'.$name.'\\'.$adapters;
-
-        if(class_exists($factory)){
-            return (new $factory(($arguments[0] ?? null)));
-        }
-
-        return throw new Exception('factory is not valid');
-    }
 }
---
+++ app/Factory/FactoryManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Factory;
+
+abstract class FactoryManager
+{
+    /**
+     * get call static for factory
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     *
+     * @throws Exception
+     */
+    public static function __callStatic(string $name,array $arguments = []): mixed
+    {
+        $name = ucfirst($name);
+        $adapters = (isset(static::$adapters[$name])) ? static::$adapters[$name] : $name;
+        $factory = 'App\Factory\\'.$name.'\\'.$adapters;
+
+        if(class_exists($factory)){
+            return (new $factory(($arguments[0] ?? null)));
+        }
+
+        return throw new Exception('factory is not valid');
+    }
+}
