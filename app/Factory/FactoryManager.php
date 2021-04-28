6c5f521dd1db6b98cc48f193b630fd99d05ad6e2
aligurbuz <galiant781@gmail.com>
2021-04-28 21:04:09 +0000

    factory fix

--- app/Factory/FactoryManager.php
+++ app/Factory/FactoryManager.php
@@ -2,7 +2,7 @@
 
 namespace App\Factory;
 
-use JetBrains\PhpStorm\Pure;
+use Illuminate\Support\Str;
 
 abstract class FactoryManager
 {
@@ -17,7 +17,8 @@ abstract class FactoryManager
      */
     public static function __callStatic(string $name,array $arguments = []): mixed
     {
-        $factory = 'App\Factory\\'.ucfirst($name).'\\'.static::getAdapterName($name);
+        $name = ucfirst($name);
+        $factory = 'App\Factory\\'.$name.'\\'.static::getAdapterName($name);
 
         if(class_exists($factory)){
             return (new $factory(($arguments[0] ?? null)));
@@ -29,12 +30,12 @@ abstract class FactoryManager
     /**
      * get adapter name for factory model
      *
-     * @param $name
+     * @param string $name
      * @return string
      */
-    #[Pure] private static function getAdapterName(string $name) : string
+    private static function getAdapterName(string $name) : string
     {
-        $name = ucfirst($name);
-        return (isset(static::$adapters[$name])) ? ucfirst(static::$adapters[$name]) : $name;
+        $adapterName = Str::lower($name);
+        return (isset(static::$adapters[$adapterName])) ? static::$adapters[$adapterName] : $name;
     }
 }
