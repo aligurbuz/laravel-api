69ce7a5d69681d0e3a853f24c893679e3c81f8f7
aligurbuz <galiant781@gmail.com>
2021-04-28 20:14:56 +0000

    factory fix

--- app/Factory/Owner/Owner.php
+++ app/Factory/Owner/Owner.php
@@ -6,17 +6,5 @@ use App\Factory\Owner\Interfaces\OwnerInterface;
 
 class Owner extends OwnerSupport implements OwnerInterface
 {
-    /**
-     * @var mixed|null
-     */
-    protected mixed $bind;
-
-    /**
-     * Owner constructor.
-     * @param null $bind
-     */
-    public function __construct($bind = null)
-    {
-        $this->bind = $bind;
-    }
+    //
 }
--- app/Factory/Owner/OwnerSupport.php
+++ app/Factory/Owner/OwnerSupport.php
@@ -4,5 +4,17 @@ namespace App\Factory\Owner;
 
 class OwnerSupport
 {
+    /**
+     * @var mixed|null
+     */
+    protected mixed $bind;
 
+    /**
+     * Owner constructor.
+     * @param null $bind
+     */
+    public function __construct($bind = null)
+    {
+        $this->bind = $bind;
+    }
 }
