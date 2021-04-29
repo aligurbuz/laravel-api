5de0c722f08ca08b0919db82d7da7b514f76151b
aligurbuz <galiant781@gmail.com>
2021-04-29 07:19:59 +0000

    content type request format

--- app/Http/Middleware/ContentType.php
+++ app/Http/Middleware/ContentType.php
@@ -29,7 +29,7 @@ class ContentType
     {
         $contentType = $request->headers->get('content-type');
 
-        if(!in_array($contentType,$this->validContentTypes) && !app()->runningInConsole()){
+        if(!in_array($contentType,$this->validContentTypes)){
            Exception::contentTypeException('',['key' => implode(',',$this->validContentTypes)]);
         }
 
