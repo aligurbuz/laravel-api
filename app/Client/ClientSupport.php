7dd5e3c9f936d2de299eecd81c43d612633640c8
aligurbuz <galiant781@gmail.com>
2021-05-07 14:19:32 +0000

    client support fix

--- app/Client/ClientSupport.php
+++ app/Client/ClientSupport.php
@@ -25,8 +25,11 @@ trait ClientSupport
     public function isDefault(): ?string
     {
         if($this->isDefault == '1'){
-            $this->repository()->update([['is_default' => '0']],false);
+            return $this->ensureColumnExists('is_default',function(){
+                $this->repository()->update([['is_default' => '0']],false);
+            });
         }
+
         return $this->isDefault;
     }
 }
