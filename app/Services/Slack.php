6919ff40cd82b15509f38fc45c0a05158394190c
aligurbuz <galiant781@gmail.com>
2021-05-09 14:42:22 +0000

    slack formatting

---
+++ app/Services/Slack.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+
+class Slack
+{
+    /**
+     * @var string
+     */
+    protected static string $hook;
+
+    /**
+     * @var string
+     */
+    protected static string $channel;
+
+    /**
+     * specifying channel for slack
+     *
+     * @param string $channel
+     * @return Slack
+     */
+    public static function channel($channel='default'): Slack
+    {
+        //we get configuration settings for slack.
+        $config = config('slack.'.$channel);
+
+        //channel and hook information.
+        self::$channel = $config['channel'];
+        self::$hook = $config['hook'];
+
+        //return static object
+        return new self();
+    }
+
+    /**
+     * push message to slack
+     *
+     * @param $message
+     * @return bool
+     */
+    public static function push($message): bool
+    {
+        // for push notification
+        // we have to specify the payload value.
+        $data = "payload=" . json_encode(array(
+                "channel"       =>  "#".self::$channel,
+                "text"          =>  $message,
+            ));
+
+        // We are starting curl for the hook value.
+        $ch = curl_init(self::$hook);
+
+        // We are entering our curl option values for push notification.
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        //finally,return result
+        return $result == 'ok';
+    }
+}
---
+++ config/slack.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'logger' => [
+        'channel' => 'logger',
+        'service' => 'https://hooks.slack.com/services/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',
+    ]
+];
