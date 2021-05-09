130148d8986792ab72688992e8c4acc1354bb432
aligurbuz <galiant781@gmail.com>
2021-05-09 14:52:59 +0000

    service slack fix

--- config/slack.php
+++ config/slack.php
@@ -3,6 +3,6 @@
 return [
     'logger' => [
         'channel' => 'logger',
-        'hook' => 'https://hooks.slack.com/services/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',
+        'hook' => 'hook',
     ]
 ];
