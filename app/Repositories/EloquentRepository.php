5924d332e64e9f103f15fc3d0638624da18d8ef6
aligurbuz <galiant781@gmail.com>
2021-05-09 19:49:56 +0000

    eloquent repository psr use statement fix

--- app/Repositories/EloquentRepository.php
+++ app/Repositories/EloquentRepository.php
@@ -2,9 +2,9 @@
 
 namespace App\Repositories;
 
+use App\Services\Db;
 use App\Services\Client;
 use App\Exceptions\Exception;
-use App\Services\Db;
 use Illuminate\Database\Eloquent\Builder;
 
 class EloquentRepository
