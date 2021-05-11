76280c1e0c64cac0fb110c6d74dbdd9af966bc3c
aligurbuz <galiant781@gmail.com>
2021-05-11 09:06:46 +0000

    index columns

--- database/columns/users.php
+++ database/columns/users.php
@@ -2,5 +2,5 @@
         'columns' => ["id","name","email","email_verified_at","password","remember_token","created_at","updated_at","username","USER","CURRENT_CONNECTIONS","TOTAL_CONNECTIONS"],
         'indexes' => ["id","email"],
         'types' => ["integer","string","string","timestamp","string","string","timestamp","timestamp","string","string","integer","integer"],
-        'required_columns' => ["name","email","password","CURRENT_CONNECTIONS","TOTAL_CONNECTIONS"],
+        'required_columns' => ["name","email","password"],
         ];
