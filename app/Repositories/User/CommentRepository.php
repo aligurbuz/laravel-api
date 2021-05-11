a926f55b3dd3304d2697e840ce5b9742c4b22dcc
aligurbuz <galiant781@gmail.com>
2021-05-11 10:10:49 +0000

    comment repository fix

--- app/Repositories/User/CommentRepository.php
+++ app/Repositories/User/CommentRepository.php
@@ -4,9 +4,29 @@ namespace App\Repositories\User;
 
 use App\Models\User_comment;
 use App\Repositories\EloquentRepository;
+use Illuminate\Database\Eloquent\Builder;
 use App\Repositories\User\Contracts\CommentRepositoryContract;
 
 class CommentRepository extends EloquentRepository implements CommentRepositoryContract
 {
+    /**
+     * @var string
+     */
 	protected static string $model = User_comment::class;
+
+    /**
+     * @var array|string[]
+     */
+    protected array $ranges = [];
+
+    /**
+     * get auto user range method
+     *
+     * @param Builder $builder
+     * @return Builder
+     */
+    public function userRepository(Builder $builder) : Builder
+    {
+        return $builder;
+    }
 }
