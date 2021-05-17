34464d4bfb996a59f49fb2b2301e933e6a878960
root <root@localhost>
2021-05-17 20:29:55 +0000

    documentation fix

--- app/Console/Commands/Documentation.php
+++ app/Console/Commands/Documentation.php
@@ -111,10 +111,10 @@ class Documentation extends Command
         $list['item'][$key]['item'][0]['request']['url']['raw'] = '{{baseUrl}}/'.$endpoint;
         $list['item'][$key]['item'][0]['request']['url']['host'] = ["{{baseUrl}}"];
         $list['item'][$key]['item'][0]['request']['url']['path'] = explode('/',$endpoint);
-        $list['item'][$key]['item'][0]['request']['url']['query'][]['key'] = 'range';
-        $list['item'][$key]['item'][0]['request']['url']['query'][]['value'] = 'desc';
-        $list['item'][$key]['item'][0]['request']['url']['query'][]['description'] = '';
-        $list['item'][$key]['item'][0]['request']['url']['query'][]['disabled'] = true;
+        $list['item'][$key]['item'][0]['request']['url']['query'][0]['key'] = 'range';
+        $list['item'][$key]['item'][0]['request']['url']['query'][0]['value'] = 'desc';
+        $list['item'][$key]['item'][0]['request']['url']['query'][0]['description'] = '';
+        $list['item'][$key]['item'][0]['request']['url']['query'][0]['disabled'] = true;
 
         $list['item'][$key]['item'][1]['name'] = $endpointName;
         $list['item'][$key]['item'][1]['response'] = [];
