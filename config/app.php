c06161d98e99af08a0075007780a0c124d5353c8
aligurbuz <galiant781@gmail.com>
2021-05-24 13:27:24 +0000

    clockwork removal

--- config/app.php
+++ config/app.php
@@ -177,8 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\RepositoryServiceProvider::class,
-        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class
 
     ],
 
