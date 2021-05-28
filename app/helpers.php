1c16c255aab401688070882fb47b24093cc9ff8e
aligurbuz <galiant781@gmail.com>
2021-05-28 15:51:12 +0000

    helper fix

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,7 @@
 
 use JetBrains\PhpStorm\Pure;
 use App\Models\Entities\EntityMap;
+use Illuminate\Support\Facades\DB;
 
 if(!function_exists('entity')){
 
@@ -13,5 +14,21 @@ if(!function_exists('entity')){
     #[Pure] function entity(): EntityMap
     {
         return new EntityMap();
+    }
+}
+
+if(!function_exists('fullTextSearchTable')){
+
+    /**
+     * full text search table for migration
+     *
+     * @param $table
+     * @param array $columns
+     * @return void
+     */
+    function fullTextSearchTable($table,array $columns = []): void
+    {
+        $queryString = 'ALTER TABLE '.$table.' ADD FULLTEXT fulltext_index ('.implode(',',$columns).')';
+        DB::statement($queryString);
     }
 }
