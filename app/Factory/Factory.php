e635126ec048c09c8c76b7b0cce600222a62a7cd
aligurbuz <galiant781@gmail.com>
2021-06-02 15:13:35 +0000

    factory fix

--- app/Factory/Factory.php
+++ app/Factory/Factory.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Factory;
 
-use App\Factory\Logger\Interfaces\LoggerInterface;
 use App\Factory\Slack\Interfaces\SlackInterface;
+use App\Factory\Logger\Interfaces\LoggerInterface;
 use App\Factory\Storage\Interfaces\StorageInterface;
 use App\Factory\Repository\Interfaces\RepositoryInterface;
 
