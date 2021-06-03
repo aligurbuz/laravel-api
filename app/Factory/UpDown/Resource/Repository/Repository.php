6c817cd5767936bdcb2c851b2e55e65a75a4adfa
aligurbuz <galiant781@gmail.com>
2021-06-03 15:22:36 +0000

    updown factory repository resource formatting

---
+++ app/Factory/UpDown/Resource/Repository/Repository.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Factory\UpDown\Resource\Repository;
+
+/**
+ * Class Repository
+ * @package App\Factory\UpDown\Resource\Repository
+ */
+class Repository
+{
+
+}
