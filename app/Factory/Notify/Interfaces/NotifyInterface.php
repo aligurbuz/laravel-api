dffe96710a5051e1deb2dc034b0de22f9e9d08eb
aligurbuz <galiant781@gmail.com>
2021-06-04 11:26:46 +0000

    notify interface see formatting

--- app/Factory/Notify/Interfaces/NotifyInterface.php
+++ app/Factory/Notify/Interfaces/NotifyInterface.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace App\Factory\Notify\Interfaces;
 
+use App\Factory\Notify\Slack;
+
 interface NotifyInterface
 {
     /**
      * @param string $channel
      * @param string $message
+     * @see Slack::push()
      */
     public function push(string $channel,string $message) : void;
 
     /**
      * @return void
+     * @see Slack::internalServerError()
      */
     public function internalServerError() : void;
 }
