cd5c9055327004627531eafbda9e10e0e7125079
aligurbuz <galiant781@gmail.com>
2021-06-12 19:11:34 +0000

    accept language fix

--- app/Http/Middleware/AcceptLanguage.php
+++ app/Http/Middleware/AcceptLanguage.php
@@ -20,20 +20,51 @@ class AcceptLanguage
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        $contentType = $request->headers->get('accept-language');
+        // It checks the accept-language value sent
+        // by the client and throws an exception if necessary.
+        $acceptLanguage = $this->checkAcceptLanguage($request);
 
-        if(is_null($contentType)){
+        // it checks the existence of the repository class according
+        // to the accept-language value and makes an sql query and returns code value
+        $code = $this->checkRepository($acceptLanguage);
+
+        //we assign the language_code value as the application container value.
+        AppContainer::set(Constants::acceptLanguage,$code);
+
+        return $next($request);
+    }
+
+    /**
+     * check content type for middleware
+     *
+     * @param Request $request
+     * @return string|null
+     */
+    private function checkAcceptLanguage(Request $request) : ?string
+    {
+        $acceptLanguage = $request->headers->get('accept-language');
+
+        if(is_null($acceptLanguage)){
             Exception::customException('Accept-Language must be sent in the header request');
         }
 
+        return $acceptLanguage;
+    }
+
+    /**
+     * check repository for middleware
+     *
+     * @param ?string $contentType
+     * @return array
+     */
+    private function checkRepository(?string $contentType) : array
+    {
         $repository = Repository::language()->findByName($contentType)->getRepository();
 
         if(!isset($repository[0]['language_code'])){
             Exception::customException('Accept-Language is not valid');
         }
 
-        AppContainer::set(Constants::acceptLanguage,$repository[0]['language_code']);
-
-        return $next($request);
+        return $repository[0]['language_code'];
     }
 }
