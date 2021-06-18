9b5b01c85f3158d746dba35e755302347b0cc729
aligurbuz <galiant781@gmail.com>
2021-06-18 09:05:05 +0000

    localization doc fix

--- database/columns/localizations.php
+++ database/columns/localizations.php
@@ -1,7 +1,7 @@
 <?php return [
         'columns' => ["created_at","created_by","deleted_at","deleted_by","id","is_deleted","language_code","localization_code","localized_code","status","updated_at","updated_by","values"],
         'indexes' => ["id","language_code","localized_code","localization_code","language_code","localized_code","status","is_deleted"],
-        'types' => ["timestamp","integer","timestamp","integer","integer","integer","integer","integer","integer","integer","timestamp","integer","json"],
+        'types' => ["timestamp","integer","timestamp","integer","integer","integer","integer","integer","integer","integer","timestamp","integer","array"],
         'required_columns' => ["language_code","localized_code","values"],
         'max_length_columns' => [],
         'max_length_values' => [],
