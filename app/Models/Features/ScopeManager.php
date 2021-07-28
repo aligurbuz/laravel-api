e54fd2a64cde86792dccf5bb55892e593c8a9734
aligurbuz <galiant781@gmail.com>
2021-07-28 13:03:12 +0000

    scope manager fix

--- app/Models/Features/ScopeManager.php
+++ app/Models/Features/ScopeManager.php
@@ -276,7 +276,12 @@ trait ScopeManager
 
             foreach ($doesntHaveQuery as $doesnt){
                 if(isset($withQuery[$doesnt],$withQuery[$doesnt]['nested']) && false===$withQuery[$doesnt]['nested']){
-                    $builder->doesntHave($doesnt);
+                    $builder->doesntHave($doesnt,function(object $builder) use($params,$doesnt){
+                        $range = $params['hasRange'][$doesnt] ?? ($params['range'] ?? '');
+                        $repository = getModelWithPlural($doesnt);
+                        $builder->range(Repository::$repository(),(string)$range);
+                        return $builder;
+                    });
                 }
             }
         }
