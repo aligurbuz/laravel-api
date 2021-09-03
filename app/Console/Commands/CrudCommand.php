a128db8b4d48dd44dee1d960648a9f95f885bde6
aligurbuz <galiant781@gmail.com>
2021-09-03 08:12:39 +0000

    crud command fix

--- app/Console/Commands/CrudCommand.php
+++ app/Console/Commands/CrudCommand.php
@@ -41,9 +41,9 @@ class CrudCommand extends Command
     {
         Artisan::call('create:controller',['controller' => $this->argument('controller'),'dir' => $this->argument('dir')]);
         //Artisan::call('create:request',['name' => $this->argument('controller')]);
-        Artisan::call('create:repository',['repository' => $this->argument('controller'),'dir' => $this->argument('dir'),'model' => $this->argument('model')]);
         Artisan::call('create:model',['model' => $this->argument('model')]);
         Artisan::call('update:column',['model' => $this->argument('model')]);
+        Artisan::call('create:repository',['repository' => $this->argument('controller'),'dir' => $this->argument('dir'),'model' => $this->argument('model')]);
         Artisan::call('create:client',['dir' => $this->argument('dir'),'client' => $this->argument('controller'),'method' => 'get','model' => $this->argument('model')]);
         Artisan::call('create:client',['dir' => $this->argument('dir'),'client' => $this->argument('controller'),'method' => 'create','model' => $this->argument('model')]);
         Artisan::call('create:client',['dir' => $this->argument('dir'),'client' => $this->argument('controller'),'method' => 'update','model' => $this->argument('model')]);
