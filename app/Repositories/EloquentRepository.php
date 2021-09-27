e78c7e1fbfcefd28ad23e8427f66cdf193d5e53d
aligurbuz <galiant781@gmail.com>
2021-09-27 10:17:51 +0000

    eloquent repository fix

--- app/Repositories/EloquentRepository.php
+++ app/Repositories/EloquentRepository.php
@@ -325,7 +325,8 @@ class EloquentRepository
      */
     public function exists($field,$value) : bool
     {
-        return !is_null($this->instance()->where($field,$value)->first());
+        $query = $this->instance()->where($field,$value)->get()->toArray();
+        return isset($query[0]);
     }
 
     /**
