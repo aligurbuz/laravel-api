3584d3fd22620ead713f82d4bd27feb28cf63b0d
aligurbuz <galiant781@gmail.com>
2021-09-28 13:19:54 +0000

    resource repository fix

--- app/Repositories/ResourceRepository.php
+++ app/Repositories/ResourceRepository.php
@@ -162,6 +162,25 @@ trait ResourceRepository
                                     $this->findRepositoryByModel(getModelWithPlural($withValue))
                                 )[0]) ?? [];
                         }
+
+                        if($this->getModelName()=='MenuItem'){
+                            if(is_array($withData)){
+                                foreach ($withData as $withDataKey => $withDatum){
+                                    if(is_array($withDatum) && count($withDatum) && isset($withData[$withDataKey.'_code'])){
+                                        $withDataModel = Str::camel($withDataKey);
+                                        if(isset($item[$withValueSnake][$withKey][$withDataKey]) && is_array($item[$withValueSnake][$withKey][$withDataKey])){
+                                            foreach ($item[$withValueSnake][$withKey][$withDataKey] as $recursiveKey => $recursiveVal){
+                                                $item[$withValueSnake][$withKey][$withDataKey][$recursiveKey] = ($this->resourcePropagation(
+                                                        [$recursiveVal],
+                                                        $this->findRepositoryByModel($withDataModel)
+                                                    )[0]) ?? [];
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
                     }
 
                 }
