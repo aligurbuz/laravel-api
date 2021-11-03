e0bb067cd25730017e667a45d3c94751010dd44f
aligurbuz <galiant781@gmail.com>
2021-11-03 12:44:43 +0000

    array rules formatting

---
+++ app/Client/ArrayRules.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Client;
+
+/**
+ * Class ArrayRules
+ * @package App\Client
+ */
+class ArrayRules
+{
+    /**
+     * get array rules for client
+     *
+     * @return \array[][]
+     */
+    public function handle() : array
+    {
+        return [
+          'countries' => [
+              'items' => [
+                  'name' => [
+                      'description' => '',
+                      'required' => false,
+                      'type' => 'string'
+                  ]
+              ]
+          ]
+        ];
+    }
+}
