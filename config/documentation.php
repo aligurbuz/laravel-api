6b741d321598fd248ba8c0b6676b6fd9dbb8f2a5
aligurbuz <galiant781@gmail.com>
2021-11-10 11:36:14 +0000

    documentation definition fix

--- config/documentation.php
+++ config/documentation.php
@@ -6,5 +6,8 @@ return [
           //'all' => 'bb', //all key : all http methods
           //'GET' => 'xx',
       ]
-  ]
+  ],
+    'definitions' => [
+        "Currencies" => "Currency is a medium of exchange for goods and services. In short, it's money, in the form of paper or coins, usually issued by a government and generally accepted at its face value as a method of payment."
+    ]
 ];
