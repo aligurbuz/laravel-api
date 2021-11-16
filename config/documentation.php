5d7ebe45d71f3291196cddd5237acea408d947d1
aligurbuz <galiant781@gmail.com>
2021-11-16 08:03:33 +0000

    documentation fix

--- config/documentation.php
+++ config/documentation.php
@@ -8,7 +8,9 @@ return [
       ]
   ],
     'definitions' => [
-        "Currencies" => "Currency is a medium of exchange for goods and services. In short, it's money, in the form of paper or coins, usually issued by a government and generally accepted at its face value as a method of payment."
+        "Currencies" => "Currency is a medium of exchange for goods and services. In short, it's money, in the form of paper or coins, usually issued by a government and generally accepted at its face value as a method of payment.",
+        "Timezones" => "The term time zone can be used to describe several different things, but mostly it refers to the local time of a region or a country.",
+
     ],
     'exceptMethods' => [
         //'all' => ['GET','PUT'],
