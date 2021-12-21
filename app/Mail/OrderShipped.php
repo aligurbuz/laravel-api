00de124fb03dd84361266cbeadcf1363be719656
aligurbuz <galiant781@gmail.com>
2021-12-21 13:08:25 +0000

    mail formatting

---
+++ app/Mail/OrderShipped.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipped extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from('alibilgegurbuz@gmail.com','Bronx Detail')
+            ->to(['galiant781@gmail.com'])
+            ->view('mail.order-shipped')->subject('test message');
+    }
+}
