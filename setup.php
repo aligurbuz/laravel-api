25fd583cf828bbc7cf4ebd34df3bb2e50d5e9b53
aligurbuz <galiant781@gmail.com>
2021-12-29 08:59:28 +0000

    setup fix

--- setup.php
+++ setup.php
@@ -4,6 +4,6 @@
 $envFile = __DIR__.'/.env';
 
 if(!file_exists($envFile)){
-    copy('/var/www/html/app/.env.example','/var/www/html/app/.env');
-    exec('cd /var/www/html/app && composer install && chmod -R 777 storage && php artisan key:generate');
+    copy(__DIR__.'/.env.example',$envFile);
+    exec('cd '.__DIR__.' && composer install && chmod -R 777 storage && php artisan key:generate');
 }
