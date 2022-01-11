2465297c2016b761ba7bd984ed6b26d0c8c825e1
aligurbuz <galiant781@gmail.com>
2022-01-11 12:22:58 +0000

    migration for registration has been created

---
+++ database/migrations/2022_01_11_122155_create_registrations.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('registration_code')->default(0)->unique();
+            $table->json('user')->comment('user data for registration');
+
+            //$table->char('column')->comment('');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->index(['status','is_deleted']);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('registrations','registrations','registration');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
