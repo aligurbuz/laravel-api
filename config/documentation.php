9b9cf88b7d655fecfb98c831d1c8ecdd15daa905
aligurbuz <galiant781@gmail.com>
2022-01-12 09:52:00 +0000

    documentation formatting

--- config/documentation.php
+++ config/documentation.php
@@ -10,12 +10,13 @@ return [
     'definitions' => [
         "Currencies" => "Currency is a medium of exchange for goods and services. In short, it's money, in the form of paper or coins, usually issued by a government and generally accepted at its face value as a method of payment.",
         "Timezones" => "The term time zone can be used to describe several different things, but mostly it refers to the local time of a region or a country.",
+        "Registration" => "The system is the service that makes user and other critical records.",
 
     ],
     'exceptMethods' => [
         //'all' => ['GET','PUT'],
         //'countries/districts' => ['GET','PUT','POST'],
-        //'countries/districts' => ['GET','PUT','POST'],
+        'registration' => ['GET','PUT'],
     ],
     "ignores" => [
         "SuperAdmins",
