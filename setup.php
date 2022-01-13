b44112f4db9e578438cb480c34ab3f64ca47e17c
aligurbuz <galiant781@gmail.com>
2022-01-13 08:38:18 +0000

    environment fix

--- setup.php
+++ setup.php
@@ -3,12 +3,12 @@
 
 $envFile = __DIR__.'/.env';
 
-if(!file_exists($envFile) && isset($argv[1])){
+if(!file_exists($envFile) && isset($argv[1],$argv[2])){
     copy(__DIR__.'/.env.example',$envFile);
     exec('cd '.__DIR__.' && composer install && sudo chmod -R 777 storage && php artisan key:generate && php artisan create:database '.$argv[1].' && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permission && php artisan supervisor');
     exec('cd '.__DIR__.' && sudo chmod -R 777 storage');
-    if(isset($argv[2])){
-        exec('cd '.__DIR__.' && php artisan environment '.$argv[2]);
-    }
-
+    exec('cd '.__DIR__.' && php artisan environment '.$argv[2]);
+}
+else{
+    throw new Exception('your parameters is missing.(this file has 2 parameters');
 }
