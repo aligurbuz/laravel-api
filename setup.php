650a0ab46e23fb45f37728246cadcc998936f257
aligurbuz <galiant781@gmail.com>
2022-01-20 07:48:31 +0000

    setup fix

--- setup.php
+++ setup.php
@@ -5,7 +5,7 @@ $envFile = __DIR__.'/.env';
 
 if(!file_exists($envFile) && isset($argv[1],$argv[2])){
     copy(__DIR__.'/.env.example',$envFile);
-    exec('cd '.__DIR__.' && composer install && sudo chmod -R 777 storage && php artisan key:generate && php artisan create:database '.$argv[1].' && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permission && php artisan supervisor');
+    exec('cd '.__DIR__.' && composer install && sudo chmod -R 777 storage && php artisan key:generate && php artisan create:database '.$argv[1].' && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permissions && php artisan supervisor');
     exec('cd '.__DIR__.' && sudo chmod -R 777 storage');
     exec('cd '.__DIR__.' && php artisan environment '.$argv[2]);
 }
