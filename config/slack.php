a9940e3e0f10fa32c29fbd2ad29a807bd6cc89a3
aligurbuz <galiant781@gmail.com>
2022-01-24 07:58:10 +0000

    deployment formatting

--- config/slack.php
+++ config/slack.php
@@ -8,5 +8,9 @@ return [
     'error500' => [
         'channel' => 'error500',
         'hook' => env('slack500error','hook'),
+    ],
+    'deployment' => [
+        'channel' => 'deployment',
+        'hook' => env('DEPLOYMENT_HOOK',''),
     ]
 ];
