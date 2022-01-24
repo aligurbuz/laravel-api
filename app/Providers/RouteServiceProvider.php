70f63010db955b4be563a8e3c028ff9b6c0c552b
aligurbuz <galiant781@gmail.com>
2022-01-24 11:58:15 +0000

    route service provider fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,7 +35,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
+        //$this->configureRateLimiting();
 
         $this->routes(function () {
             Route::prefix('api')
