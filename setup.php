4ec24b0c24980b4aa1dff8ce32764c4e5e6dcb41
aligurbuz <galiant781@gmail.com>
2022-02-11 11:59:21 +0000

    setup.php fix

--- setup.php
+++ setup.php
@@ -5,9 +5,14 @@ $envFile = __DIR__.'/.env';
 
 if(!file_exists($envFile) && isset($argv[1],$argv[2])){
     copy(__DIR__.'/.env.example',$envFile);
-    exec('cd '.__DIR__.' && composer install && sudo chmod -R 777 storage && php artisan key:generate && php artisan create:database '.$argv[1].' && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permissions && php artisan supervisor');
-    exec('cd '.__DIR__.' && sudo chmod -R 777 storage');
-    exec('cd '.__DIR__.' && php artisan environment '.$argv[2]);
+    exec('cd '.__DIR__.'/'.$argv[1].' && sudo chmod -R 777 storage');
+    exec('cd '.__DIR__.'/'.$argv[1].' && composer install && php artisan key:generate && php artisan create:database '.$argv[1].'');
+    exec('cd '.__DIR__.'/'.$argv[1].' && php artisan environment '.$argv[2]);
+    exec('cd '.__DIR__.'/'.$argv[1].' && php artisan name '.$argv[1].'');
+    exec('cd '.__DIR__.'/'.$argv[1].' && php artisan migrate');
+    exec('cd '.__DIR__.'/'.$argv[1].' && php artisan passport:install');
+    exec('cd '.__DIR__.'/'.$argv[1].' && php artisan permission');
+    exec('cd '.__DIR__.'/'.$argv[1].' && php artisan supervisor');
 }
 else{
     throw new Exception('your parameters is missing.(this file has 2 parameters');
