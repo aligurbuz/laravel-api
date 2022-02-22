8d3468cb769b14e5447b4a9de54bec0dd9c191dd
aligurbuz <galiant781@gmail.com>
2022-02-22 14:03:36 +0000

    timezone fix

--- app/Models/Entities/Timezone.php
+++ app/Models/Entities/Timezone.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace App\Models\Entities;
 
 /**
- * @property $this abbreviation
- * @property $this created_at
- * @property $this created_by
- * @property $this deleted_at
- * @property $this deleted_by
  * @property $this id
- * @property $this is_deleted
- * @property $this status
- * @property $this timezone
  * @property $this timezone_code
- * @property $this updated_at
- * @property $this updated_by
+ * @property $this timezone
+ * @property $this abbreviation
  * @property $this utc_offset
+ * @property $this status
+ * @property $this is_deleted
+ * @property $this created_by
+ * @property $this updated_by
+ * @property $this deleted_by
+ * @property $this deleted_at
+ * @property $this created_at
+ * @property $this updated_at
  */
 class Timezone
 {
@@ -41,61 +41,6 @@ class Timezone
 
 
 	/**
-	 * get abbreviation column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function abbreviation(): mixed
-	{
-		return self::$query->abbreviation;
-	}
-
-
-	/**
-	 * get created_at column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function created_at(): mixed
-	{
-		return self::$query->created_at;
-	}
-
-
-	/**
-	 * get created_by column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function created_by(): mixed
-	{
-		return self::$query->created_by;
-	}
-
-
-	/**
-	 * get deleted_at column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function deleted_at(): mixed
-	{
-		return self::$query->deleted_at;
-	}
-
-
-	/**
-	 * get deleted_by column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function deleted_by(): mixed
-	{
-		return self::$query->deleted_by;
-	}
-
-
-	/**
 	 * get id column for database table
 	 *
 	 * @return mixed
@@ -103,39 +48,6 @@ class Timezone
 	protected static function id(): mixed
 	{
 		return self::$query->id;
-	}
-
-
-	/**
-	 * get is_deleted column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function is_deleted(): mixed
-	{
-		return self::$query->is_deleted;
-	}
-
-
-	/**
-	 * get status column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function status(): mixed
-	{
-		return self::$query->status;
-	}
-
-
-	/**
-	 * get timezone column for database table
-	 *
-	 * @return mixed
-	 */
-	protected static function timezone(): mixed
-	{
-		return self::$query->timezone;
 	}
 
 
@@ -151,13 +63,68 @@ class Timezone
 
 
 	/**
-	 * get updated_at column for database table
+	 * get timezone column for database table
 	 *
 	 * @return mixed
 	 */
-	protected static function updated_at(): mixed
+	protected static function timezone(): mixed
 	{
-		return self::$query->updated_at;
+		return self::$query->timezone;
+	}
+
+
+	/**
+	 * get abbreviation column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function abbreviation(): mixed
+	{
+		return self::$query->abbreviation;
+	}
+
+
+	/**
+	 * get utc_offset column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function utc_offset(): mixed
+	{
+		return self::$query->utc_offset;
+	}
+
+
+	/**
+	 * get status column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function status(): mixed
+	{
+		return self::$query->status;
+	}
+
+
+	/**
+	 * get is_deleted column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function is_deleted(): mixed
+	{
+		return self::$query->is_deleted;
+	}
+
+
+	/**
+	 * get created_by column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function created_by(): mixed
+	{
+		return self::$query->created_by;
 	}
 
 
@@ -173,13 +140,46 @@ class Timezone
 
 
 	/**
-	 * get utc_offset column for database table
+	 * get deleted_by column for database table
 	 *
 	 * @return mixed
 	 */
-	protected static function utc_offset(): mixed
+	protected static function deleted_by(): mixed
 	{
-		return self::$query->utc_offset;
+		return self::$query->deleted_by;
+	}
+
+
+	/**
+	 * get deleted_at column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function deleted_at(): mixed
+	{
+		return self::$query->deleted_at;
+	}
+
+
+	/**
+	 * get created_at column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function created_at(): mixed
+	{
+		return self::$query->created_at;
+	}
+
+
+	/**
+	 * get updated_at column for database table
+	 *
+	 * @return mixed
+	 */
+	protected static function updated_at(): mixed
+	{
+		return self::$query->updated_at;
 	}
 
 
--- database/columns/timezones.php
+++ database/columns/timezones.php
@@ -1,8 +1,14 @@
 <?php return [
-        'columns' => ["abbreviation","created_at","created_by","deleted_at","deleted_by","id","is_deleted","status","timezone","timezone_code","updated_at","updated_by","utc_offset"],
+        'comments' => ["","","","","","","","","","","","",""],
+        'columns' => ["id","timezone_code","timezone","abbreviation","utc_offset","status","is_deleted","created_by","updated_by","deleted_by","deleted_at","created_at","updated_at"],
         'indexes' => ["id","timezone_code","status","is_deleted"],
-        'types' => ["string","timestamp","integer","timestamp","integer","integer","integer","integer","string","integer","timestamp","integer","string"],
+        'types' => ["integer","integer","string","string","string","integer","integer","integer","integer","integer","timestamp","timestamp","timestamp"],
         'required_columns' => ["timezone"],
-        'max_length_columns' => ["abbreviation","timezone","utc_offset"],
+        'max_length_columns' => ["timezone","abbreviation","utc_offset"],
         'max_length_values' => ["255","255","255"],
+        'boolean_values' => ["status","is_deleted"],
+        'default_keys' => ["timezone_code","status","is_deleted","created_by","updated_by","deleted_by"],
+        'default_values' => ["0","1","0","0","0","0"],
+        'enum_columns' => [],
+        'enum_values' => [],
         ];
