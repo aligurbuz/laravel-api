24500510313e25b1707fe7ab8f35dc4aecb1119f
aligurbuz <galiant781@gmail.com>
2022-02-26 13:51:12 +0000

    server.sh fix

--- server.sh
+++ server.sh
@@ -5,4 +5,3 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 sudo docker-compose up -d
-sudo docker exec -it php-app php /var/www/html/app/setup.php "$1" "$2"
