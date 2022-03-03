61b40c8a553c71e8beeb5371228f1f92cde0893c
aligurbuz <galiant781@gmail.com>
2022-03-03 12:22:51 +0000

    twilio env fix

--- config/sms.php
+++ config/sms.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'twilioSid'   => env('TWILIO_SID','TWILIO_SID'),
-    'twilioToken' => env('TWILIO_AUTH_TOKEN','TWILIO_AUTH_TOKEN'),
-    'twilioPhoneNumber' => '+19035468742',
+    'twilioSid'         => env('TWILIO_SID','TWILIO_SID'),
+    'twilioToken'       => env('TWILIO_AUTH_TOKEN','TWILIO_AUTH_TOKEN'),
+    'twilioPhoneNumber' => env('TWILIO_PHONE_NUMBER','TWILIO_PHONE_NUMBER')
 ];
