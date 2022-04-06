07d0d4b8a178d369b4a8662c1b5807f5cc0e3bc4
aligurbuz <galiant781@gmail.com>
2022-04-06 06:58:23 +0000

    excell service provider formatting

--- config/app.php
+++ config/app.php
@@ -190,6 +190,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        //Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -257,6 +258,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
