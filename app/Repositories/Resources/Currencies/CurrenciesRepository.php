18f36a071f09a193547287c230f8c7d70549c839
aligurbuz <galiant781@gmail.com>
2022-05-13 19:38:23 +0000

    currency repository fix

--- app/Repositories/Resources/Currencies/CurrenciesRepository.php
+++ app/Repositories/Resources/Currencies/CurrenciesRepository.php
@@ -5,39 +5,28 @@ declare(strict_types=1);
 namespace App\Repositories\Resources\Currencies;
 
 use App\Models\Currency;
-use App\Repositories\Resources\Currencies\Contracts\CurrenciesRepositoryContract;
 use App\Repositories\EloquentRepository;
+use App\Repositories\Resources\Currencies\Events\Currencies\AfterCreate;
+use App\Repositories\Resources\Currencies\Events\Currencies\AfterUpdate;
+use App\Repositories\Resources\Currencies\Events\Currencies\BeforeCreate;
+use App\Repositories\Resources\Currencies\Events\Currencies\BeforeUpdate;
+use App\Repositories\Resources\Currencies\Contracts\CurrenciesRepositoryContract;
+use App\Repositories\Resources\Currencies\PropertyHandlers\CurrenciesPropertyHandlerTrait;
 
 class CurrenciesRepository extends EloquentRepository implements CurrenciesRepositoryContract
 {
+    use AfterCreate;
+    use AfterUpdate;
+    use BeforeCreate;
+    use BeforeUpdate;
+    use CurrenciesPropertyHandlerTrait;
+
 	/**
 	 * get model name for repository
 	 *
 	 * @var string
 	 */
 	protected static string $model = Currency::class;
-
-	/**
-	 * get client ranges for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $ranges = [];
-
-	/**
-	 * localization values for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $localization = [];
-
-	/**
-	 * hitter values for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $hitter = [];
-
 
 	/**
 	 * get auto CurrenciesRepository scope method
@@ -48,31 +37,5 @@ class CurrenciesRepository extends EloquentRepository implements CurrenciesRepos
 	public function currenciesRepository(?object $builder = null): object
 	{
 		return $this->apply($builder);
-	}
-
-
-    /**
-     * the fired event after create method for repository
-     *
-     * @param array $result
-     * @param array $clientData
-     * @return void
-     */
-	public function eventFireAfterCreate(array $result = [],array $clientData = []): void
-	{
-		//
-	}
-
-
-    /**
-     * the fired event after update method for repository
-     *
-     * @param array $result
-     * @param array $clientData
-     * @return void
-     */
-	public function eventFireAfterUpdate(array $result = [],array $clientData = []): void
-	{
-		//
 	}
 }
