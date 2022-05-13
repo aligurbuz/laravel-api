db7e0b37ad5644d331489a74ecbdbd05497622a9
aligurbuz <galiant781@gmail.com>
2022-05-13 20:08:57 +0000

    super admin repository fix

--- app/Repositories/Resources/SuperAdmins/SuperAdminsRepository.php
+++ app/Repositories/Resources/SuperAdmins/SuperAdminsRepository.php
@@ -5,27 +5,29 @@ declare(strict_types=1);
 namespace App\Repositories\Resources\SuperAdmins;
 
 use App\Models\SuperAdmin;
-use App\Exceptions\Exception;
-use App\Facades\Authenticate\ApiKey;
 use App\Repositories\EloquentRepository;
 use App\Facades\Authenticate\Authenticate;
+use App\Repositories\Resources\SuperAdmins\Events\SuperAdmins\AfterCreate;
+use App\Repositories\Resources\SuperAdmins\Events\SuperAdmins\AfterUpdate;
+use App\Repositories\Resources\SuperAdmins\Events\SuperAdmins\BeforeCreate;
+use App\Repositories\Resources\SuperAdmins\Events\SuperAdmins\BeforeUpdate;
 use App\Repositories\Resources\SuperAdmins\Contracts\SuperAdminsRepositoryContract;
+use App\Repositories\Resources\SuperAdmins\PropertyHandlers\SuperAdminsPropertyHandlerTrait;
 
 class SuperAdminsRepository extends EloquentRepository implements SuperAdminsRepositoryContract
 {
+    use AfterCreate;
+    use AfterUpdate;
+    use BeforeCreate;
+    use BeforeUpdate;
+    use SuperAdminsPropertyHandlerTrait;
+
 	/**
 	 * get model name for repository
 	 *
 	 * @var string
 	 */
 	protected static string $model = SuperAdmin::class;
-
-	/**
-	 * get client ranges for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $ranges = [];
 
 	/**
 	 * get auto SuperAdminsRepository scope method
@@ -37,31 +39,6 @@ class SuperAdminsRepository extends EloquentRepository implements SuperAdminsRep
 	{
 		return $this->apply($builder);
 	}
-
-    /**
-     * the fired event after create method for repository
-     *
-     * @param array $result
-     * @param array $clientData
-     * @return void
-     */
-    public function eventFireAfterCreate(array $result = [],array $clientData = []): void
-    {
-        //
-    }
-
-
-    /**
-     * the fired event after update method for repository
-     *
-     * @param array $result
-     * @param array $clientData
-     * @return void
-     */
-    public function eventFireAfterUpdate(array $result = [],array $clientData = []): void
-    {
-        //
-    }
 
     /**
      * get authenticated user for super admin repository
