13e03dd354b94f76cabf01d7b76aa7f61652b0fe
aligurbuz <galiant781@gmail.com>
2022-05-13 17:31:24 +0000

    repository support property handler formatting

--- app/Repositories/Resources/Support/ExcelRepository.php
+++ app/Repositories/Resources/Support/ExcelRepository.php
@@ -15,27 +15,27 @@ use App\Repositories\Resources\Support\PropertyHandlers\ExcelPropertyHandlerTrai
 
 class ExcelRepository extends EloquentRepository implements ExcelRepositoryContract
 {
-	use AfterCreate;
-	use AfterUpdate;
-	use BeforeCreate;
-	use BeforeUpdate;
+    use AfterCreate;
+    use AfterUpdate;
+    use BeforeCreate;
+    use BeforeUpdate;
     use ExcelPropertyHandlerTrait;
 
-	/**
-	 * get model name for repository
-	 *
-	 * @var string
-	 */
-	protected static string $model = Excel::class;
+    /**
+     * get model name for repository
+     *
+     * @var string
+     */
+    protected static string $model = Excel::class;
 
     /**
-	 * get auto ExcelRepository scope method
-	 *
-	 * @param object|null $builder
-	 * @return object
-	 */
-	public function excelRepository(?object $builder = null): object
-	{
-		return $this->apply($builder);
-	}
+     * get auto ExcelRepository scope method
+     *
+     * @param object|null $builder
+     * @return object
+     */
+    public function excelRepository(?object $builder = null): object
+    {
+        return $this->apply($builder);
+    }
 }
