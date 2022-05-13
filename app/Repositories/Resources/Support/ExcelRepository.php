3d456f583cc6690c59f60ea8df3488316b41c076
aligurbuz <galiant781@gmail.com>
2022-05-13 17:20:56 +0000

    repository support property handler formatting

--- app/Repositories/Resources/Support/ExcelRepository.php
+++ app/Repositories/Resources/Support/ExcelRepository.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -6,11 +6,12 @@ namespace App\Repositories\Resources\Support;
 
 use App\Models\Excel;
 use App\Repositories\EloquentRepository;
-use App\Repositories\Resources\Support\Contracts\ExcelRepositoryContract;
 use App\Repositories\Resources\Support\Events\Excel\AfterCreate;
 use App\Repositories\Resources\Support\Events\Excel\AfterUpdate;
 use App\Repositories\Resources\Support\Events\Excel\BeforeCreate;
 use App\Repositories\Resources\Support\Events\Excel\BeforeUpdate;
+use App\Repositories\Resources\Support\Contracts\ExcelRepositoryContract;
+use App\Repositories\Resources\Support\PropertyHandlers\ExcelPropertyHandlerTrait;
 
 class ExcelRepository extends EloquentRepository implements ExcelRepositoryContract
 {
@@ -18,6 +19,7 @@ class ExcelRepository extends EloquentRepository implements ExcelRepositoryContr
 	use AfterUpdate;
 	use BeforeCreate;
 	use BeforeUpdate;
+    use ExcelPropertyHandlerTrait;
 
 	/**
 	 * get model name for repository
@@ -26,71 +28,7 @@ class ExcelRepository extends EloquentRepository implements ExcelRepositoryContr
 	 */
 	protected static string $model = Excel::class;
 
-	/**
-	 * get client ranges for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $ranges = [];
-
-	/**
-	 * localization values for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $localization = [];
-
-	/**
-	 * hitter values for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $hitter = [];
-
-	/**
-	 * denied eager loadings values for repository
-	 *
-	 * @var array
-	 */
-	protected array $deniedEagerLoadings = [];
-
-	/**
-	 * get auto eager loading values for repository
-	 *
-	 * @var array
-	 */
-	protected array $autoEagerLoadings = [];
-
-	/**
-	 * it contains columns to be used by groupBy method.
-	 *
-	 * @var array
-	 */
-	protected array $groupByFields = [];
-
-	/**
-	 * it contains aggregate methods to be used together groupBy method.
-	 *
-	 * @var array
-	 */
-	protected array $groupByProcessFields = [];
-
-	/**
-	 * get additional resource for repository
-	 *
-	 * @var bool
-	 */
-	protected bool $additionalResource = false;
-
-	/**
-	 * add post query for repository
-	 *
-	 * @var array
-	 */
-	protected array $addPostQueries = [];
-
-
-	/**
+    /**
 	 * get auto ExcelRepository scope method
 	 *
 	 * @param object|null $builder
