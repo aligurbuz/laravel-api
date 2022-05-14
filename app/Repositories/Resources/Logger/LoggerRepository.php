4dc6b8afc2a37f9c6210e9fcee275099738c864b
aligurbuz <galiant781@gmail.com>
2022-05-14 21:00:08 +0000

    logger repository fix

--- app/Repositories/Resources/Logger/LoggerRepository.php
+++ app/Repositories/Resources/Logger/LoggerRepository.php
@@ -6,7 +6,12 @@ namespace App\Repositories\Resources\Logger;
 
 use App\Models\AccessLogger;
 use App\Repositories\EloquentRepository;
+use App\Repositories\Resources\Logger\Events\Logger\AfterCreate;
+use App\Repositories\Resources\Logger\Events\Logger\AfterUpdate;
+use App\Repositories\Resources\Logger\Events\Logger\BeforeCreate;
+use App\Repositories\Resources\Logger\Events\Logger\BeforeUpdate;
 use App\Repositories\Resources\Logger\Contracts\LoggerRepositoryContract;
+use App\Repositories\Resources\Logger\PropertyHandlers\LoggerPropertyHandlerTrait;
 
 /**
  * Class LoggerRepository
@@ -14,5 +19,27 @@ use App\Repositories\Resources\Logger\Contracts\LoggerRepositoryContract;
  */
 class LoggerRepository extends EloquentRepository implements LoggerRepositoryContract
 {
+    use AfterCreate;
+    use AfterUpdate;
+    use BeforeCreate;
+    use BeforeUpdate;
+    use LoggerPropertyHandlerTrait;
+
+    /**
+     * get model name for repository
+     *
+     * @var string
+     */
 	protected static string $model = AccessLogger::class;
+
+    /**
+     * get auto loggerRepository scope method
+     *
+     * @param object|null $builder
+     * @return object
+     */
+    public function loggerRepository(?object $builder = null): object
+    {
+        return $this->apply($builder);
+    }
 }
