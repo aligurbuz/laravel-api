b7b10a100db2164bcabdf0382e3aa65f393b70c9
aligurbuz <galiant781@gmail.com>
2022-05-14 07:39:39 +0000

    registration repository fix

---
+++ app/Repositories/Resources/Registration/Events/Registration/AfterUpdate.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\Resources\Registration\Events\Registration;
+
+trait AfterUpdate
+{
+	/**
+	 * event performed after repository update
+	 *
+	 * @param array $result
+	 * @param array $clientData
+	 * @return void
+	 */
+	public function eventFireAfterUpdate(array $result = [], array $clientData = []): void
+	{
+		//
+	}
+}
--- app/Repositories/Resources/Registration/RegistrationRepository.php
+++ app/Repositories/Resources/Registration/RegistrationRepository.php
@@ -6,82 +6,27 @@ namespace App\Repositories\Resources\Registration;
 
 use App\Models\Registration;
 use App\Repositories\EloquentRepository;
+use App\Repositories\Resources\Registration\Events\Registration\AfterCreate;
+use App\Repositories\Resources\Registration\Events\Registration\AfterUpdate;
+use App\Repositories\Resources\Registration\Events\Registration\BeforeCreate;
+use App\Repositories\Resources\Registration\Events\Registration\BeforeUpdate;
 use App\Repositories\Resources\Registration\Contracts\RegistrationRepositoryContract;
+use App\Repositories\Resources\Registration\PropertyHandlers\RegistrationPropertyHandlerTrait;
 
 class RegistrationRepository extends EloquentRepository implements RegistrationRepositoryContract
 {
+    use AfterCreate;
+    use AfterUpdate;
+    use BeforeCreate;
+    use BeforeUpdate;
+    use RegistrationPropertyHandlerTrait;
+
 	/**
 	 * get model name for repository
 	 *
 	 * @var string
 	 */
 	protected static string $model = Registration::class;
-
-	/**
-	 * get client ranges for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $ranges = [];
-
-	/**
-	 * localization values for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $localization = [];
-
-	/**
-	 * hitter values for repository
-	 *
-	 * @var array|string[]
-	 */
-	protected array $hitter = [];
-
-	/**
-	 * denied eager loadings values for repository
-	 *
-	 * @var array
-	 */
-	protected array $deniedEagerLoadings = [];
-
-	/**
-	 * get auto eager loading values for repository
-	 *
-	 * @var array
-	 */
-	protected array $autoEagerLoadings = [];
-
-	/**
-	 * it contains columns to be used by groupBy method.
-	 *
-	 * @var array
-	 */
-	protected array $groupByFields = [];
-
-	/**
-	 * it contains aggregate methods to be used together groupBy method.
-	 *
-	 * @var array
-	 */
-	protected array $groupByProcessFields = [];
-
-	/**
-	 * get additional resource for repository
-	 *
-	 * @var bool
-	 */
-	protected bool $additionalResource = false;
-
-	/**
-	 * add post query for repository
-	 *
-	 * @var array
-	 */
-	protected array $addPostQueries = [
-        'user' => 'user.user.create'
-    ];
-
 
 	/**
 	 * get auto RegistrationRepository scope method
@@ -92,31 +37,5 @@ class RegistrationRepository extends EloquentRepository implements RegistrationR
 	public function registrationRepository(?object $builder = null): object
 	{
 		return $this->apply($builder);
-	}
-
-
-	/**
-	 * the fired event after create method for repository
-	 *
-	 * @param array $result
-	 * @param array $clientData
-	 * @return void
-	 */
-	public function eventFireAfterCreate(array $result = [], array $clientData = []): void
-	{
-		//
-	}
-
-
-	/**
-	 * the fired event after update method for repository
-	 *
-	 * @param array $result
-	 * @param array $clientData
-	 * @return void
-	 */
-	public function eventFireAfterUpdate(array $result = [], array $clientData = []): void
-	{
-		//
 	}
 }
