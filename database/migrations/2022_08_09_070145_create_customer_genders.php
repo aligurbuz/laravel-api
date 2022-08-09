9c42645578f2921b3a0e6f169d41ef370d7c917f
aligurbuz <ali@melexsoft.com>
2022-08-09 07:05:02 +0000

    migration for customerGender has been created

---
+++ database/migrations/2022_08_09_070145_create_customer_genders.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_genders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_gender_code')->default(0)->unique();
+            $table->bigInteger('customer_code')->unique()->comment('customer code');
+
+            $table->enum('gender',['male','female'])->index()->comment('gender choosing for customer');
+
+            $table->timestamps();
+        });
+
+        pushMigration('genders','customer','customerGender');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_genders');
+    }
+};
