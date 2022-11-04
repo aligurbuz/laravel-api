1b0e26b6610e1482c663e2cd50ae476f8182ee41
aligurbuz <ali@melexsoft.com>
2022-11-04 07:40:25 +0000

    basic init

--- app/Client/Auth/Login/Create/CreateClient.php
+++ app/Client/Auth/Login/Create/CreateClient.php
@@ -15,7 +15,7 @@ class CreateClient extends Client
      *
      * @var array
      */
-    protected array $capsule = ['email', 'password', 'activation_code'];
+    protected array $capsule = ['email', 'password'];
 
     /**
      * get model entity validation
