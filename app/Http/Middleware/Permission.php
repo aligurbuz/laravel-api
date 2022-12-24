c5e12fd160ad37c4fe727f3394207599c8d633f2
aligurbuz <ali@melexsoft.com>
2022-12-24 12:30:35 +0000

    middleware permission fix

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -23,12 +23,24 @@ class Permission
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        if (config('app.permission') === true && in_array(who(), $this->apiKeys, true)) {
-            if (!Factory::permission()->checkEndpoint()) {
-                return Exception::permissionException('', endpoint());
-            }
+        if ($this->permissionCondition()) {
+            return Exception::permissionException('', endpoint());
         }
 
         return $next($request);
+    }
+
+    /**
+     * get permission condition for middleware
+     *
+     * @return bool
+     */
+    private function permissionCondition() : bool
+    {
+        return (
+            config('app.permission') === true
+            && in_array(who(), $this->apiKeys, true)
+            && !Factory::permission()->checkEndpoint()
+        );
     }
 }
