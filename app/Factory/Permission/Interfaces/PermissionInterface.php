10f1d03532e018c6630a4dff1e95734222d8eafc
aligurbuz <galiant781@gmail.com>
2022-12-26 16:48:12 +0300

    permission fix

--- app/Factory/Permission/Interfaces/PermissionInterface.php
+++ app/Factory/Permission/Interfaces/PermissionInterface.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace App\Factory\Permission\Interfaces;
 
+use App\Factory\Permission\Permission;
+
 interface PermissionInterface
 {
     /**
      * @return array
+     * @see Permission::get()
      */
     public function get(): array;
 
     /**
      * @return array
+     * @see Permission::roleFormatter()
      */
     public function roleFormatter(): array;
 
     /**
      * @return bool
+     * @see Permission::checkEndpoint()
      */
     public function checkEndpoint(): bool;
 }
