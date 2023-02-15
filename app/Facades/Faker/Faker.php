e1e3efb0c68c00e5a4f9c02a15044ff4ab8a3f04
aligurbuz <galiant781@gmail.com>
2023-02-15 15:44:18 +0300

    fix

--- app/Facades/Faker/Faker.php
+++ app/Facades/Faker/Faker.php
@@ -37,4 +37,35 @@ class Faker
     {
         return crc32(Client::fingerPrint() . '_' . time() . '_' . random_int(1, 999999));
     }
+
+    /**
+     * generate uuid string for faker facade
+     *
+     * @return string
+     */
+    public static function uuid() : string
+    {
+        return faker()->uuid;
+    }
+
+    /**
+     * generate username for faker facade
+     *
+     * @return string
+     */
+    public static function username() : string
+    {
+        return faker()->userName;
+    }
+
+    /**
+     * generate digit number for faker facade
+     *
+     * @param int $number
+     * @return int
+     */
+    public static function digitNumber(int $number = 6) : int
+    {
+        return faker()->randomNumber($number);
+    }
 }
