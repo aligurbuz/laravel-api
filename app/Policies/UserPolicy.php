bdaa3227f2c9f8f5beef9840b97cfe4cd60266af
aligurbuz <galiant781@gmail.com>
2023-02-21 15:10:11 +0300

    policy formatting

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function view(User $user, User $model): Response|bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param User $user
+     * @return Response|bool
+     */
+    public function create(User $user): Response|bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
+     */
+    public function update(User $user, User $model): Response|bool
+    {
+        return true;
+    }
+}
