694a2c71043422604a8c5bebc740623d7c5e4d1e
aligurbuz <galiant781@gmail.com>
2023-03-08 15:23:41 +0300

    feature base manager fix

--- app/Models/Features/BaseManager.php
+++ app/Models/Features/BaseManager.php
@@ -78,14 +78,6 @@ trait BaseManager
             $this->setHidden(array_merge($this->getHidden(), ['sequence', 'sequence_time']));
         }
 
-        if (property_exists($this, 'lazyLoadAppends')) {
-            foreach ($this->lazyLoadAppends as $lazyLoadAppend) {
-                if (in_array(Str::camel($lazyLoadAppend), getRanges(), true)) {
-                    $this->appends[] = $lazyLoadAppend;
-                }
-            }
-        }
-
         if ($withQueryConstructor) {
             $this->withQueryConstructor();
         }
