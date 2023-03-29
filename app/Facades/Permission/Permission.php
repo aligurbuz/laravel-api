d362e635c7b26db1a8a4f8138afb6f9565429cf9
aligurbuz <galiant781@gmail.com>
2023-03-29 15:10:48 +0300

    facade permission fix

--- app/Facades/Permission/Permission.php
+++ app/Facades/Permission/Permission.php
@@ -2,7 +2,17 @@
 
 namespace App\Facades\Permission;
 
+use App\Facades\Role\Role;
+
 class Permission extends PermissionManager
 {
-
+    /**
+     * Only admins can create users.
+     *
+     * @return bool
+     */
+    public function postUser() : bool
+    {
+        return Role::isAdmin();
+    }
 }
