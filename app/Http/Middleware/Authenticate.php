63dc846a31e6861b597c22b5344f55cefb1aa882
aligurbuz <ali@melexsoft.com>
2023-04-10 20:12:57 +0000

    middleware auth fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use App\Exceptions\Exception;
 use App\Facades\Authenticate\User;
-use App\Services\AppContainer;
 use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
