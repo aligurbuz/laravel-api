e5b3d63b9bd08189bc4b1f8fb9b272bbe5afeffa
aligurbuz <galiant781@gmail.com>
2023-05-03 14:47:21 +0300

    migration for gender has been created

---
+++ database/migrations/2023_05_03_114538_create_genders.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('gender_code')->default(0)->unique();
+
+            $table->char('name')->comment('gender definition (male,femal etc..)');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('genders','definition','gender');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genders');
+    }
+};
