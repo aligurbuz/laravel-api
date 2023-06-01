cc4333792e54c60f2b109c4d0f998f7da60a84fc
aligurbuz <galiant781@gmail.com>
2023-06-01 18:12:29 +0300

    config/documentation fix

--- config/documentation.php
+++ config/documentation.php
@@ -17,7 +17,7 @@ return [
         "Support/Excel"         => 'It is the endpoint that allows you to import your Excel files into the specified tables.Your post requests are queued and your due files will be saved in the relevant tables through the queue.',
         "Password/Changes"      => 'This endpoint will send a one-time code via email or sms when the user forgets their login information.You can update your password with this code. There is a code execution time. Expired code will be unavailable. (Note: First, the POST method must be request. Sms or email will be sent via this http method. The password can be changed by sending the incoming code via PUT method) ',
         "Definition/Genders"    => 'This endpoint generally includes the genders present in the world. This table is empty by default and the customer is expected to fill it herself. The system is not responsible for unnecessary genders. All names will be registered uniquely. The service throws an exception when the same name is registered a second time.',
-
+        "Customer/Profiles"     => 'Contains the customer profile endpoint. Customers use the web part of your app. Each customer record independently includes a gender and age factor. Extra parameters are available in the instructions. Some of these may be optional. Your customer profile constitutes your portfolio in general.'
     ],
 
     'attentions' => [
