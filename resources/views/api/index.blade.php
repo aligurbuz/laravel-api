c2d80ec10947bfe8e9ba5c3eeaa7b0ffb5bfd21a
aligurbuz <ali@melexsoft.com>
2023-10-18 19:52:02 +0000

    user guide fix

--- resources/views/api/index.blade.php
+++ resources/views/api/index.blade.php
@@ -62,9 +62,10 @@ Api Documentation
 
                 <div style="position:absolute; margin-top: -20px;">
                     <h2 class="tracking-wide mb-0 mt-0 text-xs leading-loose tracking-wide text-indigo-light uppercase">
-                        <a href="guide" target="__blank">User Guide</a></h2>
-                    <h2 class="tracking-wide mb-0 mt-0 text-xs leading-loose tracking-wide text-indigo-light uppercase">
                         <a href="doc">Get Started</a></h2>
+
+                    <h2 class="tracking-wide mb-0 mt-0 text-xs leading-loose tracking-wide text-indigo-light uppercase">
+                        <a href="guide" target="__blank">User Guide</a></h2>
 
                     <h2 class="tracking-wide mb-0 mt-0 text-xs leading-loose tracking-wide text-indigo-light uppercase">
                         <a href="doc?definition=queryParams">Query Parameters</a></h2>
