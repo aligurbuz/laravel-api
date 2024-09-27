8801aea5e80645a5f8c3f73938ebf7b732186bc5
aligurbuz <ali@melexsoft.com>
2024-09-27 16:56:48 +0000

    migration for product has been created

---
+++ database/migrations/2024_09_27_165404_create_products.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_code')->default(0)->unique();
+
+            $table->char('name')->comment('product name');
+            $table->double('price')->comment('product price');
+            $table->enum('is_var', ['IN_STOCK','IN_BALANCE','OUT_DEF'])->default('IN_STOCK')->comment('product is var valuable');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('products','store','product');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
