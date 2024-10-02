f1d7937edf3112f4a9a2750519b3dc1f464a1464
aligurbuz <galiant781@gmail.com>
2024-10-02 12:00:59 +0300

    eloquent event handler formatting

--- app/Repositories/RepositoryEventHandler.php
+++ app/Repositories/RepositoryEventHandler.php
@@ -5,6 +5,8 @@ namespace App\Repositories;
 trait RepositoryEventHandler
 {
     /**
+     * It is a condition of the event capsule that is only for http.
+     *
      * @return bool
      */
     public function onlyHttpEvent(): bool
