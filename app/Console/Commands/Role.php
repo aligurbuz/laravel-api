9a2d2bd5bc74c42efa7ad4432551bf7ea069a817
aligurbuz <galiant781@gmail.com>
2024-10-09 17:11:35 +0300

    console role formatting

---
+++ app/Console/Commands/Role.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Role extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'roles';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $columns = ['role_code','role_name','is_administrator','status','is_deleted'];
+        $this->table(
+            $columns,
+            \App\Models\Role::query()->select($columns)->get(),'box-double'
+        );
+
+        return Command::SUCCESS;
+    }
+}
