94266b783a4580f5c548a8a21e10e00a3a2e770d
aligurbuz <galiant781@gmail.com>
2024-11-11 17:37:09 +0300

    horizon

--- app/Repositories/Resources/Customer/Events/Profiles/AfterCreate.php
+++ app/Repositories/Resources/Customer/Events/Profiles/AfterCreate.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace App\Repositories\Resources\Customer\Events\Profiles;
 
-use Illuminate\Support\Facades\File;
-use Laravel\SerializableClosure\Exceptions\PhpVersionNotSupportedException;
-
 trait AfterCreate
 {
     /**
@@ -15,12 +12,9 @@ trait AfterCreate
      * @param array $result
      * @param array $clientData
      * @return void
-     * @throws PhpVersionNotSupportedException
      */
     public function eventFireAfterCreate(array $result = [], array $clientData = []): void
     {
-        $this->queue('customerProfile', static function(){
-            File::put(app_path('test.json'), (string)time());
-        });
+        //
     }
 }
