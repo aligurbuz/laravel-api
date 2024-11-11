d870b5ed01e9b91e7b7e0c9d1793546f23518cec
aligurbuz <galiant781@gmail.com>
2024-11-11 17:36:49 +0300

    horizon

--- config/app.php
+++ config/app.php
@@ -207,6 +207,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class
 
