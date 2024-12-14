6a3edcf2ffd4847a0a2ac8adb6ca4c68ecbbbf19
aligurbuz <galiant781@gmail.com>
2024-12-14 14:42:54 +0300

    general fix

--- config/app.php
+++ config/app.php
@@ -210,7 +210,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
+        //App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class
 
