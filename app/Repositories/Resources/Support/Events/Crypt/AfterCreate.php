04fab4360365917417e77eb622f51be53ec97552
aligurbuz <galiant781@gmail.com>
2024-12-14 17:02:25 +0300

    crypt fix

--- app/Repositories/Resources/Support/Events/Crypt/AfterCreate.php
+++ app/Repositories/Resources/Support/Events/Crypt/AfterCreate.php
@@ -8,18 +8,18 @@ use App\Libs\HashGenerator;
 
 trait AfterCreate
 {
-	/**
-	 * event performed after repository create
-	 *
-	 * @param array $result
-	 * @param array $clientData
-	 * @return array
-	 */
-	public function eventFireAfterCreate(array $result = [], array $clientData = []): array
-	{
+    /**
+     * event performed after repository create
+     *
+     * @param array $result
+     * @param array $clientData
+     * @return array
+     */
+    public function eventFireAfterCreate(array $result = [], array $clientData = []): array
+    {
         $hash = (new HashGenerator());
-		$result['encrypt_body'] = $hash->encode($result['body']);
-		$result['decrypt_body'] = $hash->decode($result['encrypt_body'],true);
+        $result['encrypt_body'] = $hash->encode($result['body']);
+        $result['decrypt_body'] = $hash->decode($result['encrypt_body'], true);
 
         unset($result['body']);
         unset($result['api_key']);
@@ -28,5 +28,5 @@ trait AfterCreate
         unset($result['created_at']);
 
         return $result;
-	}
+    }
 }
