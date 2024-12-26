0050146585c8d095bf6ea845c162982855afbd27
aligurbuz <galiant781@gmail.com>
2024-12-26 18:09:05 +0300

    server.sh

--- server.sh
+++ server.sh
@@ -4,4 +4,4 @@ sudo chmod +x docker-install.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-sudo docker-compose up -d
+sudo docker-compose up -d --build
