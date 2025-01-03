0ce21c958fb3ed804b533f59cc3d9752a4a6ce4e
aligurbuz <ali@melexsoft.com>
2025-01-03 15:35:50 +0000

    migration for userPassword has been created

---
+++ database/migrations/2025_01_03_152842_create_user_passwords.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_passwords', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_password_code')->default(0)->unique();
+            $table->bigInteger('user_code')->comment('user code');
+
+            $table->char('password')->comment('user password value');
+            $table->timestamp('expired_at')->comment('user password expired_at value');
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+
+            $table->index(['user_code','status']);
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->timestamps();
+        });
+
+        pushMigration('password','user','userPassword');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_passwords');
+    }
+};
