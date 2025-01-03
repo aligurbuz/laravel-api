3301b59bf300fc7aedbe2cc65358800384a92149
aligurbuz <ali@melexsoft.com>
2025-01-03 20:03:52 +0000

    migration for dummy has been created

---
+++ database/migrations/2025_01_03_200113_create_dummies.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dummies', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('dummy_code')->default(0)->unique();
+
+            $table->char('endpoint_id')->comment('endpoint id for dummy data');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->bigInteger('created_by')->default(0);
+            $table->timestamps();
+        });
+
+        pushMigration('dummy','support','dummy');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dummies');
+    }
+};
