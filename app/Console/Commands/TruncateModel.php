de4c1cff1292beb63fc89252046241eecf8f27e1
aligurbuz <galiant781@gmail.com>
2025-02-05 16:55:51 +0300

    truncate model console

---
+++ app/Console/Commands/TruncateModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exceptions\Exception;
+use App\Libs\Db;
+use App\Repositories\Repository;
+use Illuminate\Console\Command;
+
+class TruncateModel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'truncate:model';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Truncate model';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        if(!app()->isLocal()){
+            Exception::customException('onlyLocal');
+        }
+
+        $model = $this->ask('model');
+
+        $code = Repository::$model()->getModelCode();
+        $relations = Db::relationCodes();
+
+        foreach ($relations[$code] as $relation){
+            $table = getModelInstance($relation)->getTable();
+            \Illuminate\Support\Facades\DB::table($table)->truncate();
+        }
+
+        $this->output->success('The related tables have been truncated');
+    }
+}
