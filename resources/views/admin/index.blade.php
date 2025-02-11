301b94fc1e7b67346c153b28b43b2e61e2235f84
aligurbuz <galiant781@gmail.com>
2025-02-11 19:25:33 +0300

    index menu

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -54,7 +54,7 @@
             <!-- Logo -->
             <!-- ============================================================== -->
             <div class="navbar-header">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="{{route('admin.dashboard.index')}}">
                     <!-- Logo icon --><b>
                         <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                         <!-- Dark Logo icon -->
