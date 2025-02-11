2aadc217c12da59bcfa3cc2c70b66a9a12fab3c5
aligurbuz <galiant781@gmail.com>
2025-02-11 18:58:24 +0300

    postman

--- app/Libs/Postman.php
+++ app/Libs/Postman.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Libs;
 
 use Illuminate\Support\Facades\File;
+use JsonException;
 
 class Postman
 {
@@ -12,17 +13,20 @@ class Postman
      * get postman environment data
      *
      * @return array
+     * @throws JsonException
      */
     public static function environment(): array
     {
-        $headerJsonFile = app_path('Docs') . '' . DIRECTORY_SEPARATOR . 'header.json';
-        return json_decode(File::get($headerJsonFile), true);
+        $headerJsonFile = app_path('Docs') . DIRECTORY_SEPARATOR . 'header.json';
+
+        return json_decode(File::get($headerJsonFile), true, 512, JSON_THROW_ON_ERROR);
     }
 
     /**
      * get postman collection according to ignore
      *
      * @return array
+     * @throws JsonException
      */
     public static function getCollectionAccordingToIgnore(): array
     {
@@ -30,13 +34,9 @@ class Postman
         $collection = static::collection();
         $itemCollection = $collection['item'] ?? [];
 
-        $list = [];
-
-        foreach ($itemCollection as $key => $item) {
-            if (!in_array($item['name'], $ignore)) {
-                $list[$key] = $item;
-            }
-        }
+        $list = array_filter($itemCollection, static function ($item) use ($ignore) {
+            return !in_array($item['name'], $ignore, true);
+        });
 
         $collection['item'] = array_values($list);
 
@@ -47,10 +47,32 @@ class Postman
      * get postman collection data
      *
      * @return array
+     * @throws JsonException
      */
     public static function collection(): array
     {
-        $postmanJsonFile = base_path('postman') . '' . DIRECTORY_SEPARATOR . '' . config('app.name') . '.postman_collection.json';
-        return json_decode(File::get($postmanJsonFile), true);
+        $postmanJsonFile = base_path('postman') . DIRECTORY_SEPARATOR . config('app.name') . '.postman_collection.json';
+
+        return json_decode(File::get($postmanJsonFile), true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * @return array
+     * @throws JsonException
+     */
+    public static function menus(): array
+    {
+        $lists = static::collection();
+        $exclude = ['Auth'];
+
+        $menus = [];
+
+        foreach ($lists['item'] as $item) {
+            if (isset($item['name']) && !in_array($item['name'], $exclude, true)) {
+                $menus[] = $item['name'];
+            }
+        }
+
+        return $menus;
     }
 }
