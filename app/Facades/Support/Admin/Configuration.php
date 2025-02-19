862240a74be4b73e59d58b4ce16effbf7bfed484
aligurbuz <galiant781@gmail.com>
2025-02-19 15:38:41 +0300

    pages

---
+++ app/Facades/Support/Admin/Configuration.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Facades\Support\Admin;
+
+use App\Libs\Http\Http;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Str;
+use JsonException;
+use function request;
+
+class Configuration
+{
+    /**
+     * @param string $endpoint
+     * @return array
+     * @throws ConnectionException
+     * @throws JsonException
+     */
+    public static function handle(string $endpoint): array
+    {
+        $requestUri = Str::snake($endpoint, '/');
+        $params = request()->query->all();
+        $httpRequest = Http::get($requestUri, $params);
+
+        $model = getModelFromEndpoint($requestUri);
+        $repository = getModelInstance($model)->getRepository();
+        $columns = array_values(array_diff($repository->getColumns(), ['id', 'password']));
+        $indexes = $repository->getIndexes();
+        $indexes = array_diff(array_unique($indexes), ['id', 'password']);
+        $data = $httpRequest['resource'][0]['data'] ?? [];
+
+        if (isset($data[0])) {
+            $columns = [];
+            foreach ($data[0] as $key => $value) {
+                $columns[] = $key;
+            }
+        }
+
+        return [
+            'data' => $httpRequest,
+            'widgets' => [
+                'table' => [
+                    'columns' => $columns,
+                    'indexes' => $indexes,
+                    'data' => $data,
+                ]
+            ]
+        ];
+    }
+}
--- app/Http/Controllers/Admin/PagesController.php
+++ app/Http/Controllers/Admin/PagesController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Facades\Support\Admin\Configuration;
 use App\Http\Controllers\Controller;
-use App\Libs\Http\Http;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use JsonException;
 
 class PagesController extends Controller
@@ -22,43 +21,8 @@ class PagesController extends Controller
         return view('admin.index', ['config' => [
             'endpoint' => $endpoint,
             'method' => $request->method(),
-            'resource' => $this->data($endpoint)
+            'resource' => Configuration::handle($endpoint)
         ]
         ]);
-    }
-
-    /**
-     * @param string $endpoint
-     * @return array
-     * @throws ConnectionException
-     * @throws JsonException
-     */
-    private function data(string $endpoint): array
-    {
-        $requestUri = Str::snake($endpoint, '/');
-        $params = \request()->query->all();
-        $httpRequest = Http::get($requestUri, $params);
-
-        $model = getModelFromEndpoint($requestUri);
-        $repository = getModelInstance($model)->getRepository();
-        $columns = array_values(array_diff($repository->getColumns(), ['id', 'password']));
-        $data = $httpRequest['resource'][0]['data'] ?? [];
-
-        if (isset($data[0])) {
-            $columns = [];
-            foreach ($data[0] as $key => $value) {
-                $columns[] = $key;
-            }
-        }
-
-        return [
-            'data' => $httpRequest,
-            'widgets' => [
-                'table' => [
-                    'columns' => $columns,
-                    'data' => $data,
-                ]
-            ]
-        ];
     }
 }
