fff4d88490b286f47c3a1033541f40c66282314f
aligurbuz <galiant781@gmail.com>
2025-02-19 12:34:36 +0300

    menu include

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -233,28 +233,7 @@
         <!-- Sidebar scroll-->
         <div class="scroll-sidebar">
             <!-- Sidebar navigation-->
-            <nav class="sidebar-nav">
-                <ul id="sidebarnav">
-                    <li class="user-pro"> <a class=" waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><img src="{{\App\Libs\AppContainer::get('public_path')}}/admin/assets/images/users/1.jpg" alt="user-img" class="img-circle"><span class="hide-menu">{{$user['username']}}</span></a>
-
-                    </li>
-                    <li> <a class="waves-effect waves-dark" href="{{route('admin.dashboard.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a>
-                    </li>
-                    @foreach($menus['menus'] as $menu)
-                        <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-layout-grid2"></i><span class="hide-menu">{{$menu}}</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                @if($menus['endpoints'][$menu])
-                                    @foreach($menus['endpoints'][$menu] as $endpoint)
-                                        <li><a href="app-ticket.html">{{$endpoint}}</a></li>
-                                    @endforeach
-
-                                @endif
-                            </ul>
-                        </li>
-                    @endforeach
-
-                    </ul>
-            </nav>
+            @include('admin.menu')
             <!-- End Sidebar navigation -->
         </div>
         <!-- End Sidebar scroll-->
