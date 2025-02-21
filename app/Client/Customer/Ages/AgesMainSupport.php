2c9af6da905dd5c355d1ce9115a8ac0ff91bc47e
aligurbuz <galiant781@gmail.com>
2025-02-21 14:59:11 +0300

    helpers

--- app/Client/Customer/Ages/AgesMainSupport.php
+++ app/Client/Customer/Ages/AgesMainSupport.php
@@ -4,13 +4,5 @@ namespace App\Client\Customer\Ages;
 
 trait AgesMainSupport
 {
-    protected int $age;
-
-    /**
-     * @return int
-     */
-    protected function age(): int
-    {
-        return abs($this->get('age'));
-    }
+    //
 }
